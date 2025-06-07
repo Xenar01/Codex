95d04e8e7077e7b84e2735886bdc90ab29c3858c
Xenar01 <106644491+Xenar01@users.noreply.github.com>
2025-06-08 01:06:20 +0300

    Complete RealEstateScraper implementation

---
+++ RealEstateScraper/utils/vpn.py
@@ -0,0 +1,23 @@
+"""Utilities for configuring and running VPN via OpenVPN CLI"""
+
+import subprocess
+from typing import Optional
+
+
+_process: Optional[subprocess.Popen] = None
+
+
+def connect(config_path: str):
+    """Start OpenVPN using the given configuration file."""
+    global _process
+    _process = subprocess.Popen(["openvpn", "--config", config_path])
+    return _process
+
+
+def disconnect():
+    """Terminate the running VPN process."""
+    global _process
+    if _process and _process.poll() is None:
+        _process.terminate()
+        _process.wait()
+        _process = None
